72639e062efc194f0f187be0ceb2e872c1dcabb2
Juan_lamouroux <juan.lamouroux@jala.university>
2024-04-09 18:46:38 -0500

    Upload-branch Gitlab

--- System/Src/Main/Core/JalaU.CIS-API.System.Core.Application/obj/Debug/net8.0/JalaU.CIS-API.System.Core.Application.AssemblyInfo.cs
+++ System/Src/Main/Core/JalaU.CIS-API.System.Core.Application/obj/Debug/net8.0/JalaU.CIS-API.System.Core.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JalaU.CIS-API.System.Core.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f02e03ba013de94f2905dcd6ca130ba0024d2161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6e86fc02ecf33313e5d8e9b47ca81f07c44ff7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("JalaU.CIS-API.System.Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JalaU.CIS-API.System.Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
