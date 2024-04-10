47d6254175d66665c39db882bc6cf1db68c76d94
Juan_lamouroux <juan.lamouroux@jala.university>
2024-04-09 22:06:01 -0500

    creation class ValidatorCreateTopic

---
+++ System/Src/Main/Core/JalaU.CIS-API.System.Core.Application/Utils/ValidatorCreateTopic.cs
@@ -0,0 +1,19 @@
+//-----------------------------------------------------------------------
+// <copyright file="ValidatorCreateTopic.cs" company="Jala University">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+namespace JalaU.CIS_API.System.Core.Application;
+
+/// <summary>
+/// Class ValidatorCreateTopic responsible for validating topic creation.
+/// </summary>
+public class ValidatorCreateTopic
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidatorCreateTopic"/> class.
+    /// </summary>
+    public ValidatorCreateTopic()
+    {
+    }
+}
